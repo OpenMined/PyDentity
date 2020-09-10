dfd167e6c6860979ea77f8b819de255cd4940655
lohanspies <21362987+lohanspies@users.noreply.github.com>
2020-09-10 14:08:04 +0200

    Added startup.sh to manage aca-py startup parameters. TODO: Added webhooks support

---
+++ examples/aries-stagingnet/startup.sh
@@ -0,0 +1,20 @@
+aca-py start \
+    -it http '0.0.0.0' "$PORT" \
+    -ot http \
+    -e "$ENDPOINT" "${ENDPOINT/http/ws}" \
+    --label "$AGENT_NAME" \
+    --auto-accept-requests --auto-ping-connection \
+    --auto-respond-credential-proposal --auto-respond-credential-offer --auto-respond-credential-request --auto-store-credential \
+    --auto-respond-presentation-proposal --auto-respond-presentation-request --auto-verify-presentation \
+    --preserve-exchange-records \
+    --invite --invite-role admin --invite-label "$AGENT_NAME (admin)" \
+    --genesis-url https://raw.githubusercontent.com/sovrin-foundation/sovrin/master/sovrin/pool_transactions_sandbox_genesis \
+    --wallet-type indy \
+    --admin 0.0.0.0 $ADMIN_PORT \
+    --admin-insecure-mode \
+    --debug-connections \
+    --debug-credentials \
+    --debug-presentations \
+    --enable-undelivered-queue \
+#    --webhook-url ${BOB_WEBHOOK_URL:-$ENDPOINT:$BOB_WEBHOOK_PORT}${ALICE_WEBHOOK_URL} \
+    "$@"
