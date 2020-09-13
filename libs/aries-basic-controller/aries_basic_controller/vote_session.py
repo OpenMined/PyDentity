fd3099c7869624fd57a384a6c7ae810b72ad20d9
Tom Titcombe <t.j.titcombe@gmail.com>
2020-09-13 14:49:13 +0100

    Create SMPC Voting session class

---
+++ libs/aries-basic-controller/aries_basic_controller/vote_session.py
@@ -0,0 +1,65 @@
+"""
+Code for managing voting sessions
+"""
+from typing import List, Optional
+
+import numpy as np
+
+
+class VoteSession:
+    """
+    Base class for voting sessions.
+    """
+
+    def __init__(self, *options, vote_title: Optional[str] = None) -> None:
+        vote_values = {}
+        for i, option in enumerate(options):
+            vote_values[option] = i
+
+        self._vote_values = vote_values
+        self._vote_classes = {v: k for k, v in vote_values.items()}
+
+        self._results = None
+        self._vote_title = None
+
+    @property
+    def ongoing(self) -> bool:
+        """
+        Whether or not the vote active
+
+        Returns:
+            bool : True if no results have been calculated, otherwise False
+        """
+        return self._results is None
+
+    def __len__(self) -> int:
+        return len(self._vote_values)
+
+    def get_vote_class(self, vote_value: str) -> int:
+        return self._vote_values[vote_value]
+
+    def get_vote_value(self, vote_class: int) -> str:
+        return self._vote_classes[vote_class]
+
+    def add_results(self, results) -> None:
+        raise NotImplementedError("add_results must be implemented by base class")
+
+    def calculate_winner(self):
+        raise NotImplementedError("calculate_winner must be implemented by base class")
+
+
+class SMPCVoteSession(VoteSession):
+    def __init__(self, Q: int, *options, vote_title: str = "SMPC Vote") -> None:
+        super().__init__(*options, vote_title=vote_title)
+
+        self._Q = Q
+
+    def add_results(self, results: List[int]) -> None:
+        self._results = results
+
+    def calculate_winner(self) -> str:
+        if self.ongoing:
+            raise RuntimeError("Vote is ongoing")
+        else:
+            decrypted_vote_counts = [shares % self._Q for shares in self._results]
+            return self.get_vote(np.argmax(decrypted_vote_counts))
