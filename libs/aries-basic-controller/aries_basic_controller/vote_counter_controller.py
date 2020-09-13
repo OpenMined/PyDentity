cb128a2d0e7e4b74b001c93684845e24f014df92
Tom Titcombe <t.j.titcombe@gmail.com>
2020-09-13 14:48:56 +0100

    Create voter and vote counter SSI agents
    No messaging functionality at the moment

---
+++ libs/aries-basic-controller/aries_basic_controller/vote_counter_controller.py
@@ -0,0 +1,30 @@
+"""
+Controller for a PryVote vote counter.
+Will be moved to pryvote dir when basic-controller is pip-installable.
+"""
+from typing import List
+from uuid import uuid4, UUID
+
+from .aries_controller import AriesAgentController
+
+
+class VoterCounterController(AriesAgentController):
+    def __init__(self, Q: int, voting_session, webhook_host: str, webhook_port: int, admin_url: str, webhook_base: str = ) -> None:
+        # Voter counters can issue "has voted" credentials
+        super().__init__(webhook_host, webhook_port, admin_url, webhook_base, connections=True, messaging=True, issuer=True)
+
+        self._id = uuid4()
+        self._voter_ids = []
+        self._Q = Q
+        self._voting_session = voting_session
+        self._vote_sum = [0] * len(self._voting_session)
+
+    def receive_vote(self, voter_id: UUID, votes: List[int]) -> None:
+        if voter_id not in self._voter_ids:
+            print(f"{self._id}: Adding vote for {voter_id}")
+            self._voter_ids.append(voter_id)
+
+            for i in range(len(votes)):
+                self._vote_sum[i] += votes[i]
+        else:
+            print(f"{self._id}: {voter_id} has already voted")
---
+++ libs/aries-basic-controller/aries_basic_controller/voter_controller.py
@@ -0,0 +1,58 @@
+"""
+Controller for a PryVote voter.
+Will be moved to pryvote dir when basic-controller is pip-installable.
+"""
+from uuid import uuid4
+
+from .aries_controller import AriesAgentController
+
+
+class VoterController(AriesAgentController):
+    def __init__(self, Q: int, webhook_host: str, webhook_port: int, admin_url: str, webhook_base: str = "") -> None:
+        super().__init__(webhook_host, webhook_port, admin_url, webhook_base, connections=True, messaging=True, issuer=False)
+
+        self._id = uuid.uuid4()
+        self._Q = Q
+        self._vote_shares = None
+        self._voting_session = None
+
+    def update_voting_session(self, voting_session) -> None:
+        self._voting_session = voting_session
+
+    def update_vote(self, vote_value: str) -> None:
+        if self._voting_session is None:
+            print(f"A voting session has not been set for {self._id}")
+            return
+
+        try:
+            vote_class = self._voting_session.get_vote_class(vote_value)
+
+            onehot_votes = [0] * len(self._voting_session)
+            onehot_votes[vote_class] = 1
+
+            vote_shares = [self._encrypt_vote(binary_vote) for binary_vote in onehot_votes]
+
+             # transpose the list to get a list of 4 vote values (the four options) for each vote share piece (the vote counters)
+            self._vote_shares = list(map(list, zip(*vote_shares)))
+        except KeyError:
+            self._vote_shares = None
+
+    def _encrypt_vote(self, vote: int):
+        share_a = random.randint(-Q,Q)
+        share_b = random.randint(-Q,Q)
+        share_c = (vote - share_a - share_b) % Q
+        return (share_a, share_b,  share_c)
+
+    def send_vote(self, *parties) -> None:
+        # TODO update to vote counter connection IDs
+        if self._vote_shares is None:
+            print(f"{self._id} has not set a vote")
+            return
+
+        assert len(self._vote_shares) == len(parties)
+
+        for vote_share, party in zip(self._vote_shares, parties):
+            party.receive_vote(self._id, vote_share)
+
+        # Expire the vote session to help avoid accidental re-voting
+        self._voting_session = None
