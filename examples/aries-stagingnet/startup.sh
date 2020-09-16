42b808726ac8818c251ca7bb8e8b7208c7276f64
lohanspies <21362987+lohanspies@users.noreply.github.com>
2020-09-16 11:52:15 +0200

    Remove automatic invitation creation on startup of agents

--- examples/aries-stagingnet/startup.sh
+++ examples/aries-stagingnet/startup.sh
@@ -7,7 +7,6 @@ aca-py start \
     --auto-respond-credential-proposal --auto-respond-credential-offer --auto-respond-credential-request --auto-store-credential \
     --auto-respond-presentation-proposal --auto-respond-presentation-request --auto-verify-presentation \
     --preserve-exchange-records \
-    --invite --invite-role admin --invite-label "$AGENT_NAME (admin)" \
     --genesis-url https://raw.githubusercontent.com/sovrin-foundation/sovrin/master/sovrin/pool_transactions_sandbox_genesis \
     --wallet-type indy \
     --admin 0.0.0.0 $ADMIN_PORT \
