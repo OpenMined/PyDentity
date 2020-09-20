8bc5068ae3f55ab14a5902acb808990f9b7e75fa
H4LL <adam.hall@napier.ac.uk>
2020-09-20 21:26:33 +0100

    Account Linked

--- projects/opus-live/src/server.py
+++ projects/opus-live/src/server.py
@@ -3,6 +3,8 @@ from aries_basic_controller.aries_controller import AriesAgentController
 import qrcode
 import asyncio
 import async_timeout
+import requests
+from bs4 import BeautifulSoup
 
 WEBHOOK_HOST = "0.0.0.0"
 WEBHOOK_PORT = 8052
@@ -17,8 +19,32 @@ asyncio.get_event_loop().run_until_complete(agent_controller.listen_webhooks())
 
 
 async def hello(request):
-    return web.Response(text="Hello, world")
 
+    # ownership_proof = secrets.token_hex(16)
+    ownership_proof = "53a4198707658f2b0402af57441aa380"
+
+    str = open('templates/form.html', 'r').read()
+    str = str.replace("$$OPUS$$", ownership_proof, 2)
+
+    return web.Response(text=str, content_type='text/html')
+
+async def github_openmined_credential(request):
+    data = await request.post()
+    user = data['user']
+    ownership_proof= data['ownership_proof']
+
+    #Retrieve Page
+    http_response = requests.get("https://github.com/"+user)
+
+    #Parse Account info for ownership token
+    soup = BeautifulSoup(http_response.text)
+    ownership_token = soup.findAll("div", {"class": "user-profile-bio"})[0].findAll("div")[0].text.strip()
+    ownership_token = ownership_token.split("#OPUS ",1)[1].split("==",1)[0]
+
+    if ownership_token == ownership_proof:
+        return web.Response(text="Account Linked", content_type='text/html')
+    else:
+        return web.Response(text="Unable to link.", content_type='text/html')
 
 
 async def connect(request):
@@ -35,5 +61,6 @@ async def connect(request):
 if __name__ == '__main__':
     app = web.Application()
     app.add_routes([web.get('/', hello),
+                    web.post('/github_openmined_credential', github_openmined_credential),
                     web.get('/connect', connect)])
     web.run_app(app, path="0.0.0.0", port="5000")
