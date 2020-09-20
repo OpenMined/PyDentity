753d0ba04d95813e50d82a9627e7e91245c3a827
wip-abramson <wip.abramson@gmail.com>
2020-09-20 17:58:21 +0100

    got aiohttp endpoint working

--- projects/opus-live/src/server.py
+++ projects/opus-live/src/server.py
@@ -1,9 +1,8 @@
-from quart import Quart
+from aiohttp import web
 from aries_basic_controller.aries_controller import AriesAgentController
 import qrcode
 import asyncio
-
-app = Quart(__name__)
+import async_timeout
 
 WEBHOOK_HOST = "0.0.0.0"
 WEBHOOK_PORT = 8052
@@ -14,36 +13,27 @@ ADMIN_URL = "http://opus-agent:8051"
 agent_controller = AriesAgentController(webhook_host=WEBHOOK_HOST, webhook_port=WEBHOOK_PORT,
                                    webhook_base=WEBHOOK_BASE, admin_url=ADMIN_URL)
 
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
+asyncio.get_event_loop().run_until_complete(agent_controller.listen_webhooks())
 
-@app.route('/')
-def home():
-    return "Hello"
 
-@app.route("/connect")
-async def connect():
+async def hello(request):
+    return web.Response(text="Hello, world")
+
+
+
+async def connect(request):
     # Create Invitation
-    invite = await agent_controller.connections.create_invitation()
     # wait for the coroutine to finish
+    invite = None
+    with async_timeout.timeout(2):
+
+        invite = await agent_controller.connections.create_invitation()
+        return web.Response(text=invite["invitation_url"])
 
 
-    connection_id = invite["connection_id"]
-    inviteURL = invite['invitation_url']
-    # Link for connection invitation
-    input_data = inviteURL
-    # Creating an instance of qrcode
-    qr = qrcode.QRCode(
-            version=1,
-            box_size=10,
-            border=5)
-    qr.add_data(input_data)
-    qr.make(fit=True)
-    img = qr.make_image(fill='black', back_color='white')
-    img.save('issuer_agent_invite_QRcode.png')
-    from IPython.display import Image
-    qr_code = Image(width=400, filename='./issuer_agent_invite_QRcode.png')
-    return qr_code
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app = web.Application()
+    app.add_routes([web.get('/', hello),
+                    web.get('/connect', connect)])
+    web.run_app(app, path="0.0.0.0", port="5000")
