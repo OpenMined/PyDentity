994575c9a3ada01620db135daa4d8ad4f9972c0a
wip-abramson <wip.abramson@gmail.com>
2020-11-02 16:01:58 +0000

    update ngrok dockerfile, move controller dockerfiles to new location

--- scripts/ngrok-wait.sh
+++ scripts/ngrok-wait.sh
@@ -4,19 +4,19 @@ NGROK_NAME=${NGROK_NAME:-ngrok}
 
 echo "ngrok end point [$NGROK_NAME]"
 
-ENDPOINT=null
-while [ -z "$ENDPOINT" ] || [ "$ENDPOINT" = "null" ]
+AGENT_ENDPOINT=null
+while [ -z "$AGENT_ENDPOINT" ] || [ "$AGENT_ENDPOINT" = "null" ]
 do
     echo "Fetching end point from ngrok service"
-    ENDPOINT=$(curl --silent $NGROK_NAME:4040/api/tunnels | ./jq -r '.tunnels[0].public_url')
-    echo "ngrok end point [$ENDPOINT]"
-    if [ -z "$ENDPOINT" ] || [ "$ENDPOINT" = "null" ]; then
+    AGENT_ENDPOINT=$(curl --silent $NGROK_NAME:4040/api/tunnels | ./jq -r '.tunnels[0].public_url')
+    echo "ngrok end point [$AGENT_ENDPOINT]"
+    if [ -z "$AGENT_ENDPOINT" ] || [ "$AGENT_ENDPOINT" = "null" ]; then
         echo "ngrok not ready, sleeping 5 seconds...."
         sleep 5
     fi
 done
 
-echo "fetched end point [$ENDPOINT]"
+echo "fetched end point [$AGENT_ENDPOINT]"
 
-export ENDPOINT=$ENDPOINT
+export AGENT_ENDPOINT=$AGENT_ENDPOINT
 exec "$@"
