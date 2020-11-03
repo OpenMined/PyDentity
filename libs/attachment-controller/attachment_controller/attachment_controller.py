0d1e65059b43f3a95db5aa9a96523c4907729776
wip-abramson <wip.abramson@gmail.com>
2020-11-03 18:40:44 +0000

    update endpoint yml args

--- libs/attachment-controller/attachment_controller/attachment_controller.py
+++ libs/attachment-controller/attachment_controller/attachment_controller.py
@@ -5,8 +5,8 @@ from .protocol_controller import ProtocolController
 class AttachmentController(AriesAgentController):
 
     def __init__(self, webhook_host: str, webhook_port: int, admin_url: str, webhook_base: str = "",
-                 connections: bool = True, messaging: bool = True, issuer: bool = True):
+                 connections: bool = True, messaging: bool = True, issuer: bool = True, api_key = None):
         super().__init__(webhook_host, webhook_port, admin_url, webhook_base,
-                         connections, messaging, issuer)
+                         connections, messaging, issuer, api_key=api_key)
 
         self.protocol = ProtocolController(admin_url, self.client_session, self.connections)
