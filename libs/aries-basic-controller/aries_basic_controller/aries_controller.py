aa3bd7a394af532b07d081fadfd08344df31f218
lohanspies <21362987+lohanspies@users.noreply.github.com>
2021-03-11 00:05:04 +0200

    added reuse address and port

--- libs/aries-basic-controller/aries_basic_controller/aries_controller.py
+++ libs/aries-basic-controller/aries_basic_controller/aries_controller.py
@@ -142,7 +142,7 @@ class AriesAgentController:
         app.add_routes([web.post(self.webhook_base + "/topic/{topic}/", self._receive_webhook)])
         runner = web.AppRunner(app)
         await runner.setup()
-        self.webhook_site = web.TCPSite(runner, self.webhook_host, self.webhook_port)
+        self.webhook_site = web.TCPSite(runner, self.webhook_host, self.webhook_port, reuse_address=True, reuse_port=True)
         await self.webhook_site.start()
 
     async def _receive_webhook(self, request: ClientRequest):
