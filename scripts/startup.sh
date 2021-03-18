9fb582ce1b26d5af3dc63dde8da7f25dfeb1abb4
morrieinmaas <motechnix@gmail.com>
2021-03-18 22:19:36 +0100

    Clean up notebooks

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,5 @@
 if [[ -z "${MULTITENANT_TUTORIAL}" ]]; then
+    echo $MULTITENANT_TUTORIAL
     python3 ./scripts/parse_yml_env_variables.py -c ./configuration/aries-args-multitenant.yaml;aca-py start --arg-file /tmp/agent_conf.yml
 else
     python3 ./scripts/parse_yml_env_variables.py -c ./configuration/aries-args-advanced.yaml;aca-py start --arg-file /tmp/agent_conf.yml
