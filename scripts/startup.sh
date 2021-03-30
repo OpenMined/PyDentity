028461368de4ea5e143d4fd760788856104e3b2f
morrieinmaas <motechnix@gmail.com>
2021-03-30 08:27:51 +0000

    Fix startup params for multitenant tutorial/not multitenant tutorial
    Checks not for existence of variable but the value instead now for starting up logic

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,4 @@
-if [[ -z "${MULTITENANT_TUTORIAL}" ]]; then
+if [ "$MULTITENANT_TUTORIAL" == "MULTITENANT_TUTORIAL" ]; then
     echo $MULTITENANT_TUTORIAL
     python3 ./scripts/parse_yml_env_variables.py -c ./configuration/aries-args-multitenant.yaml;aca-py start --arg-file /tmp/agent_conf.yml
 else
