01bb8848b4d42af6eb0b246b832ac3187928b44e
morrieinmaas <motechnix@gmail.com>
2021-04-06 17:32:54 +0000

    Revert Attchment tutorial to old state.
    * works/tested in notebook

--- libs/attachment-controller/attachment_controller/attachment_controller.py
+++ libs/attachment-controller/attachment_controller/attachment_controller.py
@@ -1,14 +1,12 @@
 from aries_basic_controller.aries_controller import AriesAgentController
 from .protocol_controller import ProtocolController
 
-from dataclasses import dataclass
 
-
-@dataclass
 class AttachmentController(AriesAgentController):
 
-    def __post_init__(self):
-        super().__post_init__()
+    def __init__(self, webhook_host: str, webhook_port: int, admin_url: str, webhook_base: str = "",
+                 connections: bool = True, messaging: bool = True, issuer: bool = True, api_key = None):
+        super().__init__(webhook_host, webhook_port, admin_url, webhook_base,
+                         connections, messaging, issuer, api_key=api_key)
 
-        self.protocol = ProtocolController(
-            self.admin_url, self.client_session, self.connections)
+        self.protocol = ProtocolController(admin_url, self.client_session, self.connections)
