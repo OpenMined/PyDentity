eb9163a03fca81e5d951f3b41e2681bd593237c0
morrieinmaas <motechnix@gmail.com>
2021-04-06 16:22:22 +0000

    Refactor attachment protocol tutorial
    * TODO: release v0.4 of aries-agent-controller
      * is required because the setup pulls aries-agent-controller from pypi
      and the current version 0.3 lacks the new controller layout

--- libs/attachment-controller/attachment_controller/protocol_controller.py
+++ libs/attachment-controller/attachment_controller/protocol_controller.py
@@ -3,10 +3,15 @@ from aries_basic_controller.controllers.connections import ConnectionsController
 
 from aiohttp import ClientSession
 
+
 # This is an example for how we might extend the basic controller
 class ProtocolController(BaseController):
 
-    def __init__(self, admin_url: str, client_session: ClientSession, connections_controller: ConnectionsController):
+    def __init__(
+            self, admin_url: str,
+            client_session: ClientSession,
+            connections_controller: ConnectionsController):
+
         super().__init__(admin_url, client_session)
         self.connections = connections_controller
 
@@ -17,6 +22,4 @@ class ProtocolController(BaseController):
         path = f"/connections/{connection_id}/send-attachment"
         return await self.admin_POST(path, data=data)
 
- # async def store_attachment(self,data):
-
- 
+# async def store_attachment(self,data):
