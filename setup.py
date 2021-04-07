be333108c9155b49ba78fd865e40b2105514b30b
lohanspies <lohan.spies@didx.co.za>
2021-04-07 15:22:44 +0200

    Fixed typos and added docs/readme.md

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 import runpy
 from setuptools import setup, find_packages
 
-PACKAGE_NAME = "aries_basic_controller"
+PACKAGE_NAME = "aries_cloudcontroller"
 # version_meta = runpy.run_path("./{}/version.py".format(PACKAGE_NAME))
 # VERSION = version_meta["__version__"]
 
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         url="https://github.com/OpenMined/PyDentity/tree/master/libs/aries-basic-controller",
         packages=find_packages(),
         include_package_data=True,
-        package_data={"aries_basic_controller": ["requirements.txt"]},
+        package_data={"aries_cloudcontroller": ["requirements.txt"]},
         install_requires=parse_requirements("requirements.txt"),
         python_requires=">=3.7.3",
         classifiers=[
