03b426183929b62674e5a56f6680453d7332dd56
wip-abramson <wip.abramson@gmail.com>
2021-05-20 16:41:32 +0100

    revert aiohttp and bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def parse_requirements(filename):
 if __name__ == "__main__":
     setup(
         name=PACKAGE_NAME,
-        version="0.2.3",
+        version="0.2.4",
         description="A simple python package for controlling an aries agent through the admin-api interface",
         long_description=long_description,
         long_description_content_type="text/markdown",
