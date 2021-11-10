f697c88d26e0a90ed8988da73710fb29d4592d7b
Timo Glastra <timo@animo.id>
2021-11-10 18:09:08 +0100

    chore: update version to 3.0.2
    Signed-off-by: Timo Glastra <timo@animo.id>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def parse_requirements(filename: str):
 if __name__ == "__main__":
     setup(
         name=PACKAGE_NAME,
-        version="0.3.1",
+        version="0.3.2",
         description="A simple python package for controlling an aries agent through the admin-api interface",
         long_description=long_description,
         long_description_content_type="text/markdown",
